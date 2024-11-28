6e663285f712b3924cb5eb8ab9eda3aece44129d
joaogabri3ls <j.gabrielcruz05@gmail.com>
2024-11-28 19:41:20 -0300

    .

--- _PIM/obj/Debug/net8.0/_PIM.AssemblyInfo.cs
+++ _PIM/obj/Debug/net8.0/_PIM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_PIM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817086fd715fee3378132d1640aa6785dfc50e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231c85a5fb21576ab3eb6389c2bf72f6cf2ba241")]
 [assembly: System.Reflection.AssemblyProductAttribute("_PIM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_PIM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
